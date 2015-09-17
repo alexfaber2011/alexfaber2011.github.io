a57214a0ca38808f826eb3aa7130c416b448d1bf
Alex Faber <alexfaber@Alex-Faber-Macbook-Pro.local>
2015-09-16 19:03:37 -0500

    gol

--- controllers/SimpleCtrl.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by alexfaber on 2/12/15.
- */
-app.controller('SimpleCtrl', ['$scope', function($scope){
-    var dwarfs = ['Bashful', 'Doc', 'Dopey', 'Happy', 'Sleepy', 'Sneezy'];
-
-    return $scope.dwarfs = dwarfs;
-}]);
--- services/restService.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by alexfaber on 2/16/15.
- *
- * API Documentation: https://docs.angularjs.org/api/ngResource/service/$resource
- */
-app.factory('restService', ['$resource', function($resource){
-    return $resource('http://ip.jsontest.com/');
-}]);
